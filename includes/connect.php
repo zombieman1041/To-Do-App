c4011821bccf019c317b5e336afbee0e008f5cbf
Blake Dayman <daymanblake@gmail.com>
2015-04-14 12:29:21 -0700

    finished videos 1-8 created form css files and mysqli connection

---
+++ includes/connect.php
@@ -0,0 +1,12 @@
+<?php
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
+if ($mysqli->connect_error) {
+	# code...
+	die('Connect Error (' . $mysqli->connect_errno .')'
+		. $mysqli->connect_error);
+}
+else{
+	//echo "Connection made";
+}
+$mysqli->close();
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Simple To-Do List</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="wrap">
+		<div class="task-list">
+			<ul>
+				<?php require("includes/connect.php"); ?>
+			</ul>
+		</div>
+		<form class = "add-new-task" autocomplete="off">
+			<input type="text" name="new-task" placeholder="Add new item..."/>
+		</form>
+	</div>
+</body>
+</html>
